48ce465d003749ae530e9dc1d12af509afe3ae50
MH <michal.huras94@gmail.com>
2023-01-09 08:03:29 +0100

    Correct description

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-function countDose() {
+function calculateDose() {
     var result = (document.getElementById("patientWeight").value * document.getElementById("inputDose").value * document.getElementById("drugVolume").value) / document.getElementById("drugConcentration").value
     document.getElementById("resultDoseSpan").innerHTML = result;
     document.getElementById("singleDoseSpan").innerHTML = result / document.getElementById("dosesPerDay").value;
